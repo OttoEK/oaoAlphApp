753392c37d399cc25e81b96e551af82e401be00d
gretamalp <german.retamal.pino@gmail.com>
2015-10-08 10:08:07 -0300

    *

--- asdfUI/src/main/webapp/js/validation.js
+++ asdfUI/src/main/webapp/js/validation.js
@@ -1,12 +1,11 @@
 $("#ejemplo").validate({
-	ignore: "ignore-validate",
-	submitHandler: function(form) {
-		var params = {
-				form: form,
-				callback: function (response) {
-				}
-		};
-		$('#ssn-modal-form').closest('div.bbva-modal-body').html("<div class='bbva-color-bar-loader'><span class='bbva-blue'></span><span class='bbva-blue-1'></span><span class='bbva-blue-2'></span><span class='bbva-blue-3'></span><span class='bbva-blue-4'></span><span class='bbva-blue-5'></span></div>");
-		$('#main-container').AjaxFormSubmit(params);
-	}
+//	submitHandler: function(form) {
+//		var params = {
+//				form: form,
+//				callback: function (response) {
+//				}
+//		};
+//		$('#ssn-modal-form').closest('div.bbva-modal-body').html("<div class='bbva-color-bar-loader'><span class='bbva-blue'></span><span class='bbva-blue-1'></span><span class='bbva-blue-2'></span><span class='bbva-blue-3'></span><span class='bbva-blue-4'></span><span class='bbva-blue-5'></span></div>");
+//		$('#main-container').AjaxFormSubmit(params);
+//	}
 });
