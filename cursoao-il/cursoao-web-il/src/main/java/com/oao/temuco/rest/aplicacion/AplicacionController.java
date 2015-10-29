e334fc8778eeef7451786d55f000f9a32690cb9a
mdelanoe <mdelanoe@ZCO-C0866S1.usersad.everis.int>
2015-10-29 10:51:09 -0300

    cambios

---
+++ cursoao-il/cursoao-web-il/src/main/java/com/oao/temuco/rest/aplicacion/AplicacionController.java
@@ -0,0 +1,34 @@
+package com.oao.temuco.rest.aplicacion;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oao.temuco.dto.AplicacionDTO;
+import com.oao.temuco.servicios.AplicacionService;
+
+
+@RestController
+@RequestMapping(value = "/aplicacion")
+public class AplicacionController {
+	
+@Autowired
+AplicacionService aplicacionService;
+
+	
+	@RequestMapping(value = "/listar", method = RequestMethod.POST)
+	public List<AplicacionDTO> aplicacionListar()  {
+		return aplicacionService.listar();
+		
+	}
+	
+
+}
